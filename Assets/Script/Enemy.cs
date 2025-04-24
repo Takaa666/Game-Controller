afd15aa40e2c53f310fd177357685f43f940bb64
Jeryan Eka Yuda <jejey09@gt.student.pens.ac.id>
2025-04-24 12:22:23 +0700

    Enemy Drop
    enemy drop point

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
     public GameObject Musuh1;
+
+    [Header("Loot")]
+    public List<EnemyDrop> drop = new List<EnemyDrop>();
 
     // Metode untuk mengurangi kesehatan enemy
     public void TakeDamage(int damage)
@@ -20,6 +24,14 @@ public class Enemy : MonoBehaviour
     // Metode untuk menghancurkan enemy
     void Die()
     {
+        foreach(EnemyDrop enemyDrop in drop)
+        {
+            if(Random.Range(0f, 100f) <= enemyDrop.dropChance)
+            {
+                InstantiateLoot(enemyDrop.itemPrefab);
+            }
+            break;
+        }
         Musuh1.SetActive(false); 
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,6 +39,15 @@ public class Enemy : MonoBehaviour
         if (collision.CompareTag("Bullet"))
         {
             Die();
+        }
+    }
+
+    void InstantiateLoot(GameObject loot)
+    {
+        if (loot)
+        {
+            GameObject droppedLoot = Instantiate(loot, transform.position, Quaternion.identity);
+            droppedLoot.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 }
---
+++ Assets/Script/EnemyDrop.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDrop : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    [Range(0, 100)] public float dropChance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
